d648c07d09f913c9e88ba4857f65c063ae14fbff
HarshitChauhan <harshitkumar2503@gmail.com>
2020-06-07 19:46:38 +0530

    Updated console log

--- cli.ts
+++ cli.ts
@@ -1,11 +1,9 @@
-import {
-  countModulesInDenoLandX,
-} from "./mod.ts";
+import { countModulesInDenoLandX } from "./mod.ts";
 
 async function cli() {
   console.log(
-    "Total third party modules in https://deno.land/x :",
-    await countModulesInDenoLandX(),
+    "Total third party modules in https://deno.land/x : %d",
+    await countModulesInDenoLandX()
   );
 }
 
